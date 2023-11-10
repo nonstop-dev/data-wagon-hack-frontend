97b55c83af4ea2de89f27104f8f404f727a2ba37
asurgutov <surgutov55@gmail.com>
2023-11-11 02:48:12 +0300

    added initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { WagonCardComponent } from './components/wagon-card/wagon-card.component';
+import { SearchCardComponent } from './components/search-card/search-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WagonCardComponent,
+    SearchCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/wagon-card/wagon-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wagon-card',
+  templateUrl: './wagon-card.component.html',
+  styleUrls: ['./wagon-card.component.scss']
+})
+export class WagonCardComponent {
+
+}
