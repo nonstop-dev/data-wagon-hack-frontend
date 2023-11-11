0b76e4c409e9b444aa9f49e50eae90e15351166f
asurgutov <surgutov55@gmail.com>
2023-11-11 03:00:35 +0300

    added colors & favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home/home.component';
 import { WagonCardComponent } from './components/wagon-card/wagon-card.component';
 import { SearchCardComponent } from './components/search-card/search-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
