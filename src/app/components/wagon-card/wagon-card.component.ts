71569b09ac4ab6a7b2fef5e1399358042b5c9b82
asurgutov <surgutov55@gmail.com>
2023-11-12 03:16:44 +0300

    added train template

--- src/app/components/wagon-card/wagon-card.component.ts
+++ src/app/components/wagon-card/wagon-card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wagon-card',
   templateUrl: './wagon-card.component.html',
-  styleUrls: ['./wagon-card.component.scss']
+  styleUrls: ['./wagon-card.component.scss'],
 })
-export class WagonCardComponent {
+export class WagonCardComponent implements OnInit {
+  constructor() {}
+  public currTemplate = 'train';
 
+  ngOnInit(): void {}
 }
